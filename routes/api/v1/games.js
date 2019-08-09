73d442e8fbf0ff5b8f24434e60a543ab89edb627
Patrick Duvall <duvallp208@gmail.com>
2019-08-09 11:33:37 -0600

    Add route for one game

--- routes/api/v1/games.js
+++ routes/api/v1/games.js
@@ -14,19 +14,15 @@ router.get("/", function(req, res, next) {
     });
 });
 
-router.get("/:id", function(req, res, next) {
-  Game.findAll({
-    where:{
-      id: req.params.id
-    }
-  })
+router.get("/:id", function (req, res, next) {
+  Game.findByPk(req.params.id)
     .then(game => {
       res.setHeader("Content-Type", "application/json");
-      res.status(200).send(JSON.stringify(games));
+      res.status(200).send(JSON.stringify(game));
     })
     .catch(error => {
       res.setHeader("Content-Type", "application/json");
-      res.status(500).send({error})
+      res.status(500).send({ error })
     });
 });
 
