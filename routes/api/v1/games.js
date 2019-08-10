93b2104a7cca922bfe09a12363628cb79a2d244c
Patrick Duvall <duvallp208@gmail.com>
2019-08-10 13:54:33 -0600

    Add update route

--- routes/api/v1/games.js
+++ routes/api/v1/games.js
@@ -43,6 +43,30 @@ router.post("/", function(req, res, next) {
     });
 });
 
+router.put("/:id", function(req, res, next) {
+  Game.update({
+          title: req.body.title,
+          price: req.body.price,
+          releaseYear: req.body.releaseYear,
+          active: req.body.active
+    },
+  {
+    returning: true,
+    where: {
+      id: req.params.id
+    }
+  }
+)
+    .then(([rowsUpdate, [updatedGame]]) => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(202).send(JSON.stringify(updatedGame));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({ error });
+    });
+});
+
 
 
 
